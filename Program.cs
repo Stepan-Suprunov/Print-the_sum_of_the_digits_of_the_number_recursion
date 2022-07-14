f71cd32266ea40ed8e2a65e83a21a73cb6da4fc8
Stepan-Suprunov <ipsuprunovso@mail.ru>
2022-07-14 19:47:23 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Принять на вход число и вывести сумму чисел в нём
+
+
+int SumDigits(int a)
+{
+    if (a == 0) return 0;
+
+    return a%10 + SumDigits(a / 10);
+}
+
+int N = 4515;
+
+Console.WriteLine(SumDigits(N));
